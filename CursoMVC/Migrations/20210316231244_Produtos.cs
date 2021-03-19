723454b157a89d141fa7c5ba51e90b22d715a926
Patricia <patricia.rodrigues@mutant.com.br>
2021-03-19 20:01:48 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Migrations/20210316231244_Produtos.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoMVC.Migrations
+{
+    public partial class Produtos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CursoMVC.Models;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+            public virtual DbSet<Categoria> Categorias { get; set; }
+            public DbSet<Produto> Produtos { get; set; }
+            public Context() { }
+
+            public Context(DbContextOptions<Context> options) : base(options)
+            { }
+
+            public virtual void SetModified(object entity)
+            {
+                Entry(entity).State = EntityState.Modified;
+            }
+        }
+    }
